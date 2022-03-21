e4c4d40b973f58573c850675d0774edfcc3cba2b
Patrick Hill <graffixnyc@gmail.com>
2022-03-21 12:35:40 -0400

    Updated express-handlebars dependency for lecture 8

--- lecture_08/code/HTML_Templating/app.js
+++ lecture_08/code/HTML_Templating/app.js
@@ -9,7 +9,7 @@ app.use('/public', static);
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 configRoutes(app);
